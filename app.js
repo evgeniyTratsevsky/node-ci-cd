3a28c55ea0a6ad06fff84a7f32ff6cc891479d9f
yatr0316 <yauheni.tratseuski@netcracker.com>
2025-03-06 16:22:27 +0300

    Change app.js

--- app.js
+++ app.js
@@ -1,27 +1,7 @@
-/*
+const express = require('express')
+const app = express()
+const port = 3000
 
-GEREKLİ PAKETLER YÜKLENİYOR...
+app.get('/', (req, res) => res.send('<h3>Node CI-CD</h3>'))
 
-*/
-var http = require('http');
-var express = require('express');
-
-var app = express();
-
-app.set('port', process.env.PORT || 3005); // GİRİŞ PORTU AYARLANDI
-app.set('views', __dirname + '/app/server/views'); // VIEW KLASÖRÜ TANITILDI
-app.set('view engine', 'ejs'); // VIEW ENGINE AYARLANDI
-app.use(express.static(__dirname + '/app/public')); // KULLANICILAR TARAFINDAN ERİŞİLEBİLEN KLASÖR TANIMLANDI
-
-// require('./app/routes')(app); // ROUTE DOSYASI ÇAĞIRILDI
-
-/*
-
-HTTP SERVER OLUŞTURULDU
-
-*/
-http.createServer(app).listen(app.get('port'), function(){
-	console.log('Sistem ' + app.get('port') + ' Portu Üzerinde Çalışıyor.');
-});
-
-console.log('');
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
