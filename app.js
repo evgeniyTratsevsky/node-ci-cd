eee2f614f0d731c7cc1bcfdbb1efbc73c49c053c
yatr0316 <yauheni.tratseuski@netcracker.com>
2025-07-03 12:58:35 +0300

    change log

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('<h3>Node CI-CD</h3>'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!!`))
