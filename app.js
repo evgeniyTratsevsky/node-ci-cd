bb7c315cfc2054aa75be1fd105de969458a7fe5c
yatr <eigorevich@gmail.com>
2025-08-05 21:39:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('<h3>Node CI-CD!!!!!!!!!</h3>'))
+app.get('/', (req, res) => res.send('<h3>Node CI-CD *******************</h3>'))
 
 app.get('/health', (req, res) => {
   res.status(200).json({ status: 'healthy', timestamp: new Date().toISOString() })
